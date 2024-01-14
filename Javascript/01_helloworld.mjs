66871561f8ad74f5070fe55817406d21a44b3e59
abidkk <dev.abidkk@gmail.com>
2024-01-14 19:03:12 +0500

    modiify

--- Javascript/01_helloworld.mjs
+++ Javascript/01_helloworld.mjs
@@ -1,3 +1,24 @@
 console.log("Hello world!");
 console.log(12345);
 
+let a = "b";
+console.log(a.charAt(0));
+
+
+
+// functions 
+
+
+
+// Arrow function
+
+let sum = () =>{
+    console.log(100+300)
+}
+
+sum()
+
+
+
+
+// onsubmit return false in html form tag to avoid reloading page on form submition
