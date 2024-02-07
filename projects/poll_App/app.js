fe451872f7f3ddf26bf32bba7ac80ea18daef93b
abidkk <dev.abidkk@gmail.com>
2024-02-07 18:49:43 +0500

    local storage

---
+++ projects/poll_App/app.js
@@ -0,0 +1,101 @@
+console.log("Hello world!");
+
+const getName = document.getElementById("inputName");
+const getParty = document.getElementById("inputParty");
+const getVote = 0;
+
+let setData = document.getElementById("dataContainer");
+
+let candidateData = [];
+
+const loopHandler = () => {
+  setData.innerHTML = "";
+  const candidateList = localStorage.getItem("candidate") || "[]";
+  const parsedCandidate = JSON.parse(candidateList);
+  candidateData = parsedCandidate;
+  candidateData.map((item, index) => {
+    console.log(`From localStorage : ${item.name} and ${item.party}`);
+    console.log(`good`);
+    setData.innerHTML += ` <div class="bg-gray-200 p-2 w-64 m-5 shadow-md">
+<!-- icons -->
+<div class="flex justify-between  ">
+    <img  onclick=(editHandler(${index}))  src="./public/edit.jpg" alt="edit" class="w-6 h-6 cursor-pointer  opacity-50 hover:opacity-100 duration-500 ">
+    <img onclick=(removeHandler(${index}))  src="./public/cross2.png" alt="remove" class="w-8 h-8 cursor-pointer  opacity-40 hover:opacity-100 duration-500 ">
+</div>
+<div>
+    <h1 class="text-center py-2 text-xl"> Vote: <b class="text-red-500 "> ${item.vote} </b></h1>
+</div>
+<div class="flex justify-center">
+    <img src="./public/userlogo.png" alt="candidate_logo" class="w-24 h-24 ">
+</div>
+
+<div class="text-center  py-2">
+    <h1 class="font-bold">${item.name}</h1>
+    <h1>${item.party}</h1>
+</div>
+
+<div class="pt-2 ">
+    <button class="bg-blue-500 w-full px-4 py-1 hover:bg-blue-700 duration-500 text-white" onclick=(voteHandler(${index}))>vote</button>
+</div>
+
+</div>`;
+  });
+};
+
+// Submit handler
+const submitHandler = () => {
+  const candidate = {
+    name: getName.value,
+    party: getParty.value,
+    vote: 0,
+  };
+
+  // if (candidate.name === candidateData[1].name) {
+  //     alert("candidate already exists!")
+
+  // }else{
+  //     candidateData.push(candidate)
+  // }
+
+  candidateData.push(candidate);
+  localStorage.setItem("candidate", JSON.stringify(candidateData));
+
+  console.log(candidateData);
+  getName.value = "";
+  getParty.value = "";
+  loopHandler();
+};
+
+// vote handler
+let voteHandler = (index) => {
+  console.log("hello vote");
+  candidateData[index].vote += 1;
+  localStorage.setItem("candidate", JSON.stringify(candidateData));
+  loopHandler();
+};
+
+// remove candidate
+const removeHandler = (index) => {
+//   confirm(
+//     `Are you sure you want to remove ${candidateData[index].name} from Election?`
+//   );
+  
+  if (confirm(`Are you sure you want to remove ${candidateData[index].name} from Election?`) == true) {
+    candidateData.splice(index, 1);
+  console.log(candidateData);
+  localStorage.setItem("candidate", JSON.stringify(candidateData));
+  loopHandler();
+    // alert('deleted')
+  } else {
+    alert("canclead")
+  }
+
+};
+
+// edit candidate
+const editHandler = (index) => {
+  getName.value = candidateData[index].name;
+  getParty.value = candidateData[index].party;
+};
+
+loopHandler();
---
+++ projects/poll_App/script.js
@@ -0,0 +1,15 @@
+
+let info = [
+    
+    {name:"abid", party:"spp"},
+    {name:"aslam", party:"psp"},
+
+]
+
+if (info[0].name == "abid") {
+    console.log(1)
+}else{
+    console.log(0)
+}
+
+// info.name.includes("abid") ? console.log(1) : console.log(0)
