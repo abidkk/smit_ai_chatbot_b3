f2aa79727377317f07509e21560665e6be2d5f7b
abidkk <dev.abidkk@gmail.com>
2024-02-24 22:00:02 +0500

    hello

--- express/00_hello/index.mjs
+++ express/00_hello/index.mjs
@@ -1,14 +1,14 @@
-
 import express from 'express';
 const app = express();
-
 const PORT = 8002;
 
+// Base URL
 app.get('/', (req, res)=>{
 res.status(200).send({message: "Hello Express!"})
 })
 
 
+// listining app
 app.listen(PORT,()=>{
     console.log(`App is listening on port ${PORT}`)
 })
