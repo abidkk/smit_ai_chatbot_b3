b836d9a14263bdec955c0132d92ed1089cd21c7e
abidkk <dev.abidkk@gmail.com>
2024-03-13 17:33:05 +0500

    es6 module

---
+++ nodeJs/02_modules/01_es6_modules/footer.js
@@ -0,0 +1,4 @@
+const foot = "I am footer";
+export default foot;
+
+// Only one default export can be used in a file
---
+++ nodeJs/02_modules/01_es6_modules/home.js
@@ -0,0 +1,2 @@
+import footer from './footer.js'
+console.log(footer, "& I am in Home Page");
